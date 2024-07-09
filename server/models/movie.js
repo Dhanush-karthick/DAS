7391bc43c16c9eeac24cb8b94155aee53d907603
Dhanush-karthick <dhanush.karthick2022@vitstudent.ac.in>
2024-07-09 21:03:16 +0530

    updated movie.js

--- server/models/movie.js
+++ server/models/movie.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const MovieSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  releaseDate: { type: Date, required: true },
+  releaseDate: { type: String, required: true },
   poster: { type: String, required: true },
   rating: { type: String, default: "No rating" },
   reviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Review' }],
