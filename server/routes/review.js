a8dfef7f779b0f07a943f7e0bc91ac3e7fd233f9
Dhanush-karthick <dhanush.karthick2022@vitstudent.ac.in>
2024-07-09 20:54:49 +0530

    update

--- server/routes/review.js
+++ server/routes/review.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { createReview, getReviewsByMovie } = require('../controllers/review');
-const authMiddleware = require('../middlewares/authMiddleware');
+const authMiddleware = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
